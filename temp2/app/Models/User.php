9d53a8e75e4bf4259969b3876bba45cd748b8bdc
nicole <user.email>
2024-04-23 17:33:50 +0800

    API GATEWAY

---
+++ temp2/app/Models/User.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    protected $table = "tbluser";
+
+    // column sa table
+    protected $fillable = ["username", "password"];
+
+    public $timestamps = false;
+
+    protected $primaryKey = "id";
+
+    protected $hidden = ["password"];
+}
